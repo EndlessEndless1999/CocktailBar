55ea14790e83af84061cc731b2a5f66d3629995c
EndlessEndless1999 <115580045+EndlessEndless1999@users.noreply.github.com>
2023-02-08 15:27:13 +0000

    Adds Cocktail API integration with two settings

---
+++ assets/script.js
@@ -0,0 +1,35 @@
+
+
+let searchedDrink = '20th Century';
+
+//At the moment this gets many cocktails.
+const Settings = {
+    default : {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://drinks-digital1.p.rapidapi.com/v1/cocktails?limit=20",
+        "method": "GET",
+        "headers": {
+            "X-RapidAPI-Key": "776d092347msh3e7fd81bb3c4eaap19f0c5jsn6f3d75095a68",
+            "X-RapidAPI-Host": "drinks-digital1.p.rapidapi.com"
+        }
+    },
+    search : {
+        "async": true,
+	    "crossDomain": true,
+	    "url": "https://drinks-digital1.p.rapidapi.com/v1/cocktails/search?query=" + searchedDrink + "&limit=20",
+	    "method": "GET",
+	    "headers": {
+		    "X-RapidAPI-Key": "776d092347msh3e7fd81bb3c4eaap19f0c5jsn6f3d75095a68",
+		    "X-RapidAPI-Host": "drinks-digital1.p.rapidapi.com"
+	    }
+    }
+
+};
+
+let currentSetting = Settings.search;
+
+
+$.ajax(currentSetting).done(function (response) {
+	console.log(response);
+});
