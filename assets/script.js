50e61c85621fabb18b20a932a30c241265ad25d0
EndlessEndless1999 <115580045+EndlessEndless1999@users.noreply.github.com>
2023-02-09 15:35:31 +0000

    adds local business API

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 
 
 let searchedDrink = '20th Century';
+let ingredient = 'milk';
 let drinkResults;
 
 //At the moment this gets many cocktails.
@@ -28,9 +29,46 @@ const Settings = {
 
 };
 
+const allergenAPISettings = {
+    parser: { "async": true,
+	"crossDomain": true,
+	"url": "https://edamam-food-and-grocery-database.p.rapidapi.com/parser?ingr=" + ingredient,
+	"method": "GET",
+	"headers": {
+		"X-RapidAPI-Key": "776d092347msh3e7fd81bb3c4eaap19f0c5jsn6f3d75095a68",
+		"X-RapidAPI-Host": "edamam-food-and-grocery-database.p.rapidapi.com"
+	}}
+}
+
+const localBusinessSettings = {
+    search: { "async": true,
+	"crossDomain": true,
+	"url": "https://local-business-data.p.rapidapi.com/search?query=" + barSearch + '&limit=20' + '&lat=' + latitude + '&lng=' + longitude + '&zoom=13&region=en&language=en',
+	"method": "GET",
+	"headers": {
+		"X-RapidAPI-Key": "776d092347msh3e7fd81bb3c4eaap19f0c5jsn6f3d75095a68",
+		"X-RapidAPI-Host": "local-business-data.p.rapidapi.com"}
+    }
+}
+
+//Stores which setting is selected
 let currentSetting = Settings.search;
+let currentAllergenSetting = allergenAPISettings.parser;
+let currentBusinessSetting = localBusinessSettings.search;
 
+function callAPI(){
+    $.ajax(currentSetting).then(function (response) {
+        console.log(response);
+    });
+}
 
-$.ajax(currentSetting).done(function (response) {
-	console.log(response);
-});
+function callAllergenAPI(){
+    $.ajax(currentAllergenSetting).then(function(response){
+        console.log(response);
+    })
+}
+
+function callBusinessAPI(){
+
+}
+
